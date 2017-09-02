16b5966c0f2ad7a7107b0835757b0e3b95ebc73a
zuochiren <1010476088@qq.com>
2017-09-02 15:00:35 +0800

    study chapter 13

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config(object):
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	FLASKY_POSTS_PER_PAGE = 20
 	FLASKY_FOLLOWERS_PER_PAGE = 50
+	FLASKY_COMMENTS_PER_PAGE = 30
 	
 	@staticmethod
 	def init_app(app):
